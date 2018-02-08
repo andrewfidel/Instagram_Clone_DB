9ee680e73428f9b2510238f624a745087654aee6
Andrew Fidel <Drew@Andrews-MacBook-Pro-2.local>
2018-02-07 19:49:18 -0800

    added photos entity

--- schema/ig_clone.sql
+++ schema/ig_clone.sql
@@ -14,3 +14,11 @@ INSERT INTO users (username) VALUES
   ('BlueCat'),
   ('CharlieBrown'),
   ('ColtSteele');
+
+  CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  );
