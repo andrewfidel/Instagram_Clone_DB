523d4d150a4d60dca6568f893d14ff8451e1f6cc
Andrew Fidel <Drew@Andrews-MacBook-Pro-2.local>
2018-02-07 21:14:27 -0800

    added hashtag=tags and photo_tags tables

--- schema/ig_clone.sql
+++ schema/ig_clone.sql
@@ -23,6 +23,22 @@ CREATE TABLE photos (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+-- Hashtag: tags, photos, photo_tags
+CREATE TABLE tags (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+  photo_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  FOREIGN KEY (photo_id) REFERENCES photos(id),
+  FOREIGN KEY (tag_id) REFERENCES tags(id),
+  PRIMARY KEY (photo_id, tag_id)
+);
+-- primary keys prevents adding a new 1-1 photo_id-tag_id duplicate to this table
+
 CREATE TABLE comments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   comment_text VARCHAR(255) NOT NULL,
